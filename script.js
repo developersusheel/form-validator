23e2e5aaac63c9d7a6e632acaeb6c6cce05b5d95
Susheel Kumar <susheel.kumar.msl@gmail.com>
2022-06-19 01:55:26 +0530

    Validation Completed

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ function showSuccess(input){
 }
 
 // Check if email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLocaleLowerCase());
+    if(re.test(input.value.trim())){
+      showSuccess(input);
+    }else{
+      showError(input, 'Email is not valid');
+    }
 }
 
 
@@ -35,6 +39,24 @@ function checkRequired(inputArr){
   });
 }
 
+// Check input length
+function checkLength(input, min, max){
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  }else if(input.value.length > max){
+    showError(input, `${getFieldName(input)} must be less then ${max} characters`);
+  }else{
+    showSuccess(input);
+  }
+}
+
+// check password match
+function checkPasswordMatch(input1, input2){
+  if(input1.value !== input2.value){
+    showError(input2, 'Password do not match');
+  }
+}
+
 // Get Fields
 function getFieldName(input){
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -45,6 +67,10 @@ form.addEventListener('submit', function(e){
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 
 
   // if(username.value === ''){
